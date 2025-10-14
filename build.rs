e5bce1527e8377cb94a5505bcdf641b905f4b39e
Abdulhakim Haliru <hakimkal@gmail.com>
2025-10-14 15:34:29 +0100

    updated signal proto

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
+        // .out_dir("src/generated")
         .file_descriptor_set_path("src/signals_descriptor.rs") // Output file for FILE_DESCRIPTOR_SET
         .compile_protos(&["proto/signals.proto"], &["proto"])?;
     Ok(())
