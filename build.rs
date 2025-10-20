a73bc7fa70ec4a48914923d608f6e19aaabccaa6
Abdulhakim Haliru <hakimkal@gmail.com>
2025-10-20 08:48:14 +0100

    added token security info

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(true)
         // .out_dir("src/generated")
         .type_attribute("signals.BuySignal", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .type_attribute("signals.TokenSecurityInfo", "#[derive(serde::Serialize, serde::Deserialize)]")
         .file_descriptor_set_path("src/signals_descriptor.bin") // Output file for FILE_DESCRIPTOR_SET
         .compile_protos(&["proto/signals.proto"], &["proto"])?;
     Ok(())
